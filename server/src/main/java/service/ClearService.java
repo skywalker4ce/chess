43a0eecdc7197e7ac6e823c2b54cd74ec5051f49
skywalker4ce <sky43@byu.edu>
2024-05-20 14:13:55 -0600

    basic structures for service classes finished (without error handling)

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,4 +1,17 @@
 package service;
 
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+
 public class ClearService {
+    MemoryUserDAO myUser = new MemoryUserDAO();
+    MemoryAuthDAO myAuth = new MemoryAuthDAO();
+    MemoryGameDAO myGame = new MemoryGameDAO();
+
+    public void clearApplication(){
+        myUser.clear();
+        myGame.clear();
+        myAuth.clear();
+    }
 }
--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -2,12 +2,38 @@ package service;
 
 import dataaccess.MemoryAuthDAO;
 import dataaccess.MemoryGameDAO;
-import dataaccess.MemoryUserDAO;
+import model.AuthData;
+import model.GameData;
+
+import java.util.ArrayList;
 
 public class GameService {
-    MemoryUserDAO myUser = new MemoryUserDAO();
     MemoryAuthDAO myAuth = new MemoryAuthDAO();
     MemoryGameDAO myGame = new MemoryGameDAO();
 
+    public ArrayList<GameData> listGames(String authToken){
+        if (myAuth.getAuth(authToken) != null){
+            return myGame.listGames();
+        }
+        else
+            return null; // CHANGE!!!
+    }
 
+    public int createGame(String authToken, String gameName){
+        if (myAuth.getAuth(authToken) != null){
+            return myGame.createGame(gameName);
+        }
+        else
+            return 0; //Change this
+    }
+
+    public void joinGame(String authToken, String playerColor, int gameID){
+        AuthData tempAuth = myAuth.getAuth(authToken);
+        if (tempAuth != null){
+            GameData tempGame = myGame.getGame(gameID);
+            if (tempGame != null){
+                myGame.updateGame(tempGame, tempAuth.username(), playerColor);
+            }
+        }
+    }
 }
