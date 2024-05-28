41ee525e9a92fd71ceeb0f5865cfe021c88f295c
skywalker4ce <sky43@byu.edu>
2024-05-28 11:54:32 -0600

    Working on creating UserService tests

--- server/src/test/java/Service/UserServiceTests.java
+++ server/src/test/java/Service/UserServiceTests.java
@@ -1,25 +1,85 @@
 package Service;
 
 
+import dataaccess.DataAccessException;
 import dataaccess.MemoryAuthDAO;
 import dataaccess.MemoryGameDAO;
 import dataaccess.MemoryUserDAO;
+import model.AuthData;
+import model.UserData;
 import org.junit.jupiter.api.*;
+import service.ClearService;
+import service.UnauthorizedException;
+import service.UserService;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserServiceTests {
     MemoryUserDAO myUser = new MemoryUserDAO();
     MemoryGameDAO myGame = new MemoryGameDAO();
     MemoryAuthDAO myAuth = new MemoryAuthDAO();
+    UserService myUserService = new UserService(myUser, myAuth);
+    ClearService myClearService = new ClearService(myUser, myAuth, myGame);
+
+    @BeforeEach
+    public void setup(){
+        myClearService.clearApplication();
+    }
+
 
 
     @Test
     @Order(1)
-    @DisplayName("Clear Test")
-    public void clearTest(){
-        myUser.createUser("Sky", "Password1234", "email@gmail.com");
-        Assertions.assertNull(myUser.getUser("ky"));
+    @DisplayName("Register Positive Test")
+    public void registerPositive() throws UnauthorizedException, DataAccessException {
+        myUserService.register(new UserData("Sky", "Password1234", "email@gmail.com"));
+        UserData testUser = myUser.getUser("Sky");
+        Assertions.assertEquals(testUser, new UserData("Sky", "Password1234", "email@gmail.com"));
     }
+
+    @Test
+    @Order(2)
+    @DisplayName("Register Negative Test")
+    public void registerNegative() throws UnauthorizedException, DataAccessException {
+        myUserService.register(new UserData("Sky", "Password1234", "email@gmail.com"));
+        try {
+            myUserService.register(new UserData("Sky", "Password1234", "email@gmail.com"));
+            fail("Expected an error");
+        }
+        catch (DataAccessException e){
+            Assertions.assertEquals("{ \"message\": \"Error: already taken\" }", e.getMessage());
+        }
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Login Positive Test")
+    public void loginPositive() throws UnauthorizedException, DataAccessException {
+        myUserService.register(new UserData("Sky", "Password1234", "email@gmail.com"));
+        AuthData testAuth = myUserService.login(new UserData("Sky", "Password1234", "email@gmail.com"));
+        AuthData loginAuth = myAuth.getAuth(testAuth.authToken());
+        Assertions.assertEquals(loginAuth.username(), "Sky");
+
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Login Negative Test")
+    public void loginNegative() throws UnauthorizedException, DataAccessException {
+        myUserService.register(new UserData("Sky", "Password1234", "email@gmail.com"));
+        try {
+            myUserService.login(new UserData("ky", "Password1234", "email@gmail.com"));
+            fail("Expected an Error");
+        }
+        catch (UnauthorizedException e){
+            Assertions.assertEquals("{ \"message\": \"Error: unauthorized\" }", e.getMessage());
+        }
+
+
+    }
+
+
 
 
 }
