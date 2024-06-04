ad9835ad4fdd9f8c29ea2ab3d6b71e9c643f85eb
skywalker4ce <sky43@byu.edu>
2024-06-04 17:01:23 -0600

    skeleton for both menus finished

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -68,6 +68,49 @@ public class Main {
     }
 
     private static void postLoginMenu(){
-
+        Scanner scanner = new Scanner(System.in);
+        menu:
+        while (true){
+            System.out.println(SET_TEXT_BOLD + "Enter a number (1-4) to continue:" + RESET_TEXT_BOLD_FAINT);
+            System.out.println("1. Create a Game");
+            System.out.println("2. List all active Games");
+            System.out.println("3. Observe a Game");
+            System.out.println("4. Join a Game");
+            System.out.println("5. Logout");
+            System.out.println("6. Help");
+            var input = scanner.next();
+            switch (input) {
+                case "1":
+                    System.out.println("Enter a name for the game: ");
+                    String gameName = scanner.next();
+                    System.out.println("This is where you would make the request to create a game " + gameName);
+                    break;
+                case "2":
+                    System.out.println("This is where you would list all active games ");
+                    break;
+                case "3":
+                    System.out.println("Enter the number of the game you would like to observe: ");
+                    String gameNumber = scanner.next();
+                    System.out.println("This is where you would find the game and display the chessboard " + gameNumber);
+                    break;
+                case "4":
+                    System.out.println("Enter the number of the game you would like to join: ");
+                    String gameNumberToJoin = scanner.next();
+                    System.out.println("This is where you would find the game, update it, and display the chessboard " + gameNumberToJoin);
+                    break;
+                case "5":
+                    break menu;
+                case "6":
+                    System.out.println("Type '1' to create a new Chess game");
+                    System.out.println("Type '2' to get a list of the current active games");
+                    System.out.println("Type '3' to watch an active game");
+                    System.out.println("Type '4' to join an active game");
+                    System.out.println("Type '5' to logout \n");
+                    break;
+                default:
+                    System.out.println("Invalid input");
+                    break;
+            }
+        }
     }
 }
